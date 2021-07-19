1c15feaa62357f087e68a091cef3ec01292977a4
Dmitry Kasporsky <31612563+KasporskiDzmitry@users.noreply.github.com>
2021-07-19 17:35:42 +0300

    generate project by spring boot

---
+++ src/main/java/by/dz/fruits_in_choco/fruits_in_choco/FruitsInChocoApplication.java
@@ -0,0 +1,13 @@
+package by.dz.fruits_in_choco.fruits_in_choco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitsInChocoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruitsInChocoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/dz/fruits_in_choco/fruits_in_choco/FruitsInChocoApplicationTests.java
@@ -0,0 +1,13 @@
+package by.dz.fruits_in_choco.fruits_in_choco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitsInChocoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

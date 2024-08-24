04145a4c078993bf2eaa3809771536b4043274f8
RomanShalkouski <ShalkovskyRG@gmail.com>
2024-08-24 19:15:49 +0300

    Added token verifying

--- fruits_in_choco_frontend/src/components/AppRouter/ProtectedRoutes.js
+++ fruits_in_choco_frontend/src/components/AppRouter/ProtectedRoutes.js
@@ -1,10 +1,18 @@
 import {Outlet} from "react-router-dom";
 import Login from "../../pages/Login/Login";
+import {useVerifyToken} from "./useVerifyToken";
 
 export const ProtectedRoutes = () => {
     const token = localStorage.getItem("token");
+    const tokenVerified = useVerifyToken(token);
+
     if (!token) {
         return <Login/>
+    }
+
+    if (!tokenVerified){
+        // clear all data
+        return <Login />
     }
 
     return <Outlet/>;
---
+++ fruits_in_choco_frontend/src/components/AppRouter/useVerifyToken.js
@@ -0,0 +1,19 @@
+import React, {useEffect, useState} from 'react';
+import RequestService from "../../api/RequestService";
+
+export const useVerifyToken = (token) => {
+
+
+    useEffect(() => {
+        if (token){
+            verifyToken(token);
+        }
+    }, [token])
+
+    const verifyToken = async () => {
+        const response = await RequestService.post('/auth/verifyToken', true);
+        console.log(response)
+    }
+
+    return true;
+};
